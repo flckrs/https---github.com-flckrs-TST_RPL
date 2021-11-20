64283ad92b78fb42753562a4ef13a33a81af274c
flckrs <steflx0510@gmail.com>
2021-11-20 11:45:35 +0700

    updated get all items

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from database import SessionLocal
+import models
 
 app = FastAPI()
 
-# Login
-# Post Data Supply
-# Delete Data Supply
-# Edit Data Supply
-# Read Data Supply
+# Login (Login tinggal ambil)
+# Post Data Supply (Post)
+# Delete Data Supply (Delete)
+# Edit Data Supply (Put)
+# Read Data Supply (Get One/All)
 
 class Item(BaseModel):
   id: int
@@ -16,20 +18,34 @@ class Item(BaseModel):
   description:str
   price: int
   on_offer: bool
+  
+  class Config:       #serialize sql algorithm to json
+    orm_mod=True
+
+db = SessionLocal()
 
 @app.get('/')
 def index(): 
-  return {"Welcome"}
+  return {"Welcome":"Hello :p"}
 
-@app.put('/item/{item_id}')
-def update_item(item_id:int, item:Item):
-  return {'name': item.name,
-          'desciption': item.description,
-          'price': item.price,
-          'on_offer': item.on_offer
-          }
+@app.get('/items', response_model=List[Item], status_code=200)
+def get_all_items():
+  items=db.query(models.Item).all()
 
+  return items
 
-@app.post('item/{item_id}')
-def add_item():
+@app.get('/item/{item_id}')
+def get_an_item(item_id: int):
+  return 0
+
+@app.post('/items')
+def create_an_item():
+  return 0
+
+@app.put ('/item/item_id}')
+def update_an_item(item_id:int):
+  return 0
+
+@app.delete('/item/{item_id}')
+def delete_item(item_id:int):
   return 0
--- models.py
+++ models.py
@@ -6,4 +6,8 @@ class Item(Base):
   id= Column(Integer, primary_key=True)
   name= Column(String(255), nullable= False, unique=True)
   description= Column(Text)
+  price=Column(Integer, nullable=False)
   on_offer=Column(Boolean, default=False)
+
+  def __repr__(self):
+    return f"<Item name={self.name} price={self.price}>"
