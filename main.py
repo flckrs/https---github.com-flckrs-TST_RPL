ffe0682a13f8fcdef78aefe769e394e11efa6e73
flckrs <steflx0510@gmail.com>
2021-11-20 10:04:49 +0700

    added dependencies

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+# Login
+# Post Data Supply
+# Delete Data Supply
+# Edit Data Supply
+# Read Data Supply
+
+class Item(BaseModel):
+  id: int
+  name: str
+  description:str
+  price: int
+  on_offer: bool
+
 @app.get('/')
-def index():
+def index(): 
   return {"Welcome"}
+
+@app.put('/item/{item_id}')
+def update_item(item_id:int, item:Item):
+  return {'name': item.name,
+          'desciption': item.description,
+          'price': item.price,
+          'on_offer': item.on_offer
+          }
+
+
+@app.post('item/{item_id}')
+def add_item():
+  return 0
